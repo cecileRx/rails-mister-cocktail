c3f2d088e9028d94ece78ee57ea525d903ea1685
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2019-11-14 19:10:25 +0100

    almost

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "apple")
+Ingredient.create(name: "coconut")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "tomato")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "water")
+
