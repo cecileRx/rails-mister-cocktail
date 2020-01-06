6dcb7ad154390eaeefa68d34535d501e2070e884
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-01-06 21:39:06 +0100

    new dose presentation

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,36 +1,31 @@
 class DosesController < ApplicationController
-
-  def index
-    @doses = Dose.all
-  end
-
-  def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
-    @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      @review = Review.new
+      render "cocktails/show"
     end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
-    @cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
-
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
+
